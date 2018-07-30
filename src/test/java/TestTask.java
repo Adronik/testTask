8624e2b68f0494c88d50c20beb8fc7e160fc405c
Alexander <alexander.pustovoy@dataart.com>
2018-07-30 13:32:22 +0300

    test setup check

--- src/test/java/TestTask.java
+++ src/test/java/TestTask.java
@@ -3,7 +3,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+public class TestTask {
 
     @BeforeMethod
     public void beforeEachTest() {
@@ -12,7 +12,7 @@ public class LoginTest {
 
     @Test
     public void gmailLogin() {
-        
+
     }
 
     @AfterMethod
